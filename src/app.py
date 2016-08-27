5552bf3196a960d9dac1a69f3147a813f10875ea
Curtis Sand <curtis@curtissand.com>
2016-08-27 10:37:35 -0600

    app: refactor app code into a class to appease pylint

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+"""The netify application object."""
 # Copyright 2015 Curtis Sand
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,24 +12,24 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-"""The netify application object."""
-
-import os
-
 from flask import Flask
 
-DEFAULT_SECRET_KEY_SIZE = 64  # bytes -> 64 * 8 = 512bits
 
+class NetifyApp(object):
+    """The Netify Application object."""
+    flask_app = None
 
-APP = None  # Singleton Flask App
+    def __init__(self, config=None):
+        if self.flask_app is None:
+            self.__class__.flask_app = Flask(__name__)
+        if config and self.flask_app:
+            config.update_flask(self.flask_app)
 
-def init():
-    global APP
-    APP = Flask(__name__)
-    APP.config.from_object(__name__)
-    APP.config.update(dict(SECRET_KEY=os.urandom(DEFAULT_SECRET_KEY_SIZE)))
-    return APP
+    def register_views(self, views):
+        """Register the view classes against the flask app."""
+        for view in views:
+            view.register(self.flask_app)
 
-def run(host=None, port=None, debug=None):
-    APP.run(host, port, debug)
+    def run(self, host=None, port=None, debug=None):
+        """Run the Flask Server."""
+        self.flask_app.run(host, port, debug)

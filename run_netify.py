e30399c7b45cc29401db61c564b7ad9097053019
Curtis Sand <curtis@curtissand.com>
2016-08-30 23:21:09 -0600

    run_netify.py: refactor dev run script to be all standards compliant
    It even works too.

--- run_netify.py
+++ run_netify.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+"""The development netify CLI script."""
 # Copyright 2015 Curtis Sand
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,14 +13,24 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import sys
 
-"""The netify CLI script."""
+# Modify the path to add the "src" directory. This lets netify be imported
+# later down in the protected "__main__" block below.
+sys.path.insert(0, './src')
 
-try:  #  use installed netify package if available
+
+def run_netify():
+    """Run the CLI Main method for Netify.
+
+    This is encapsulated in a method to appease the pylint gods. At the module
+    level we need to add the "src" directory to the path so no more module
+    level imports are allowed. Using a method we squish the import scope and
+    get to drink our flask too.
+    """
     from netify import cli_main
-except ImportError:  # use src package in repository instead
-    from src import cli_main
+    cli_main()
 
 
 if __name__ == "__main__":
-    cli_main()
+    run_netify()

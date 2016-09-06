fa357f3eb74e684d30cddbedefa4216453b54f5a
Curtis Sand <curtis.sand@emc.com>
2016-09-06 14:46:25 -0600

    style.sh: ignore some pylint messages for now
    Pylint doesn't seem to like the way flask extensions install themselves.
    I believe I have seen a flask-pylint extension that may help resolve
    that issue. For now I'll just disable the two error messages.

--- style.sh
+++ style.sh
@@ -17,4 +17,4 @@
 echo "[PEP8]"
 find . -iname "*py" -exec pep8 '{}' '+'
 echo "[Pylint]"
-PYTHONPATH=. pylint src/netify run_netify.py
+PYTHONPATH=. pylint --disable no-name-in-module,import-error src/netify run_netify.py

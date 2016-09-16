30b6f2093214de413248bbc3f4dab456e6dc7a5d
Curtis Sand <curtis@curtissand.com>
2016-09-15 22:05:30 -0600

    tests app.py: test NetifyCore class
    I've left out the test implementation for the register views method
    because I have a feeling that there is a bug in the method currently. It
    will be looked at at a later time.

--- src/netify/tests/app.py
+++ src/netify/tests/app.py
@@ -13,24 +13,76 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from unittest import main
+from unittest import skip
+from unittest.mock import Mock
+from unittest.mock import patch
 
 from flask_testing import TestCase
 
+import netify
 import netify.app as app
+import netify.config as config
 
 
-class TestApp(TestCase):
+class TestNetifyCore(TestCase):
     """Tests for the netify.app module."""
 
     def create_app(self):
         """Create an instance of the flask_app for the Flask Testing API."""
-        netify = app.NetifyApp()
-        return netify.flask_app
+        napp = app.NetifyApp()
+        return napp.flask_app
+
+    def test_singleton(self):
+        """Verify that NetifyApp is a signleton."""
+        self.assertEqual(app.NetifyApp().flask_app, self.app)
+
+    @staticmethod
+    @patch('netify.app.NetifyApp.configure')
+    def test_init_config(mock_configure):
+        """Test that the configure method is called passed to constructor."""
+        app.NetifyApp(config=Mock())
+        mock_configure.assert_called()
 
     def test_description(self):
         """Verify the string description property of the netify object."""
-        netify = app.NetifyApp()
-        self.assertIsInstance(netify.description, str)
+        napp = app.NetifyApp()
+        self.assertIsInstance(napp.description, str)
+
+    @patch('netify.config.Config.update_flask')
+    @patch('netify.config.SafeConfigParser', Mock())
+    def test_configure_path(self, mock_update_flask):
+        """Verify the configure method accepts a config path."""
+        path = 'some/fake/path'
+        napp = app.NetifyApp()
+        napp.configure(path)
+        self.assertIsInstance(napp.config, config.Config)
+        self.assertTrue(mock_update_flask.called)
+
+    @patch('netify.config.Config.update_flask')
+    @patch('netify.config.SafeConfigParser', Mock())
+    def test_configure_obj(self, mock_update_flask):
+        """Verify the configure method accepts a config object."""
+        config_obj = config.Config('blah/blah/blah')
+        napp = app.NetifyApp()
+        napp.configure(config_obj)
+        self.assertEqual(napp.config, config_obj)
+        self.assertTrue(mock_update_flask.called)
+
+    @staticmethod
+    @patch.object(netify.app.NetifyApp, 'flask_app')
+    def test_run(mflask_app):
+        """Verify the run method behaves as expected."""
+        host = 'host'
+        port = 8080
+        debug = True
+        napp = app.NetifyApp()
+        napp.run(host, port, debug)
+        mflask_app.run.assert_called_once_with(host, port, debug)
+
+    @skip('Not implemented yet')
+    def test_register_views(self):
+        """Verify the process of registering Flask views."""
+        pass
 
 
 if __name__ == "__main__":
